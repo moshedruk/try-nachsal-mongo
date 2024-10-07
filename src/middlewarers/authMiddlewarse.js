a8d4fc3867e34cbc7a924013f65b2df67427e215
moshedruk <dmd6708713@gmail.com>
2024-10-07 11:41:48 +0300

    create-middlewarese

---
+++ src/middlewarers/authMiddlewarse.js
@@ -0,0 +1,19 @@
+
+// Middleware for validating if user is a commander
+const onlyCommaders = async (req,res,next) => {
+    try{
+    }
+    catch(err){        
+    }
+};
+
+
+
+// Middleware for validating if user is a solders and if they are commander
+
+const onlySellersAndCommanders = async (req,res,next) => {
+    try{        
+    }
+    catch(err){        
+    }
+};
