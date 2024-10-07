3b69367b14cb749495324f686a20fc1ef20f470a
moshedruk <dmd6708713@gmail.com>
2024-10-07 10:31:55 +0300

    connect-app-Routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 const dotenv = require('dotenv');
 const exp = require('express');
+const auth = require('./routes/authRouter');
+const user = require('./routes/userRouter');
+const greenEye = require('./routes/greenEetRouter');
+
 
 const app = exp();
 dotenv.config();
 const port  = process.env.PORT
 
 
+app.use(exp.json())
+
+// Routes
+app.use('/auth', auth)
+app.use('/user', user)
+app.use('/greenEye',greenEye )
+
+// Server running message
 app.listen(port,() => console.log(' listening on port ' + port));
